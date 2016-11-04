2cc76537c64361d36edc878a206f885407a4797d
Tomas Gunther <tomasgunther@gmail.com>
2016-11-04 12:03:32 -0300

    validaciones email

--- app/models/spree/newslatter_user.rb
+++ app/models/spree/newslatter_user.rb
@@ -1,3 +1,7 @@
 class Spree::NewslatterUser < ActiveRecord::Base
   has_and_belongs_to_many :taxons, join_table: :spree_newslatter_user_taxons
+
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  validates :email , email: true
 end
