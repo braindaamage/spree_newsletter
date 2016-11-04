21184bd59109208907c7c39b6f8e75175eb45dab
Tomas Gunther <tomasgunther@gmail.com>
2016-11-04 14:27:36 -0300

    fix CSRF token

--- app/controllers/spree/newslatter_controller.rb
+++ app/controllers/spree/newslatter_controller.rb
@@ -1,4 +1,5 @@
 class Spree::NewslatterController < ApplicationController
+  skip_before_filter  :verify_authenticity_token, only: :create
 
   def create
     @newslatter_user = Spree::NewslatterUser.new(newslatter_user_params)
